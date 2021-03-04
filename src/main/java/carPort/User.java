de92107de0f551b8db67a6340eca921bdaeb913d
wolfs <wolfs@txzing.com>
2021-03-04 12:51:49 +0800

    更新相应接口

--- src/main/java/carPort/User.java
+++ src/main/java/carPort/User.java
@@ -14,8 +14,18 @@ import java.util.Map;
 public interface User {
 
     /**
+     * 发送意见邮件
+     * @param title 发送的标题
+     * @param content 发送的内容
+     * @return 返回发送状态
+     */
+    @RequestMapping("sendEmail/{title}/{content}")
+    public Map<String,Object> sendEmail(String title , String content);
+
+
+
+    /**
      * 用户登录使用
-     * @param session
      * @param code
      * @return
      */
@@ -65,5 +75,81 @@ public interface User {
      */
     @RequestMapping("findCarPoolingByTrip/{origin}/{bourn}")
     public Map<String,Object> findCarPoolingByTrip();
+
+
+
+    /**
+     * 发起拼车 填写发起拼车信息
+     * @param origin 出发地
+     * @param bourn 目的地
+     * @param readyTime 最早出发时间
+     * @param GoTime    最晚出发时间
+     * @param totalNum  拼车总人数
+     * @param getNum    需要拼车的人数
+     * @param inCarMsg  车主在拼车中的留言——为空则为null
+     * @param QQNum     QQ号——为空则为null
+     * @param WXNum     微信号——为空则为null
+     * @param phoneNum  手机号——为空则为null
+     * @param email 拼车结果通知邮箱——为空则为null
+     * @return 返回拼车状态
+     */
+    @RequestMapping("createCarPooling/{origin}/{bourn}/{readyTime}/{GoTime}/{totalNum}/{getNum}/{inCarMsg}/{QQNum}/{WXNum}/{phoneNum}/{email}")
+    public Map<String,Object> createCarPooling(String origin,String bourn,String readyTime,String GoTime,String totalNum,String getNum,String inCarMsg,String QQNum,String WXNum,String phoneNum,String email);
+
+
+
+
+    /**
+     * 加入拼车 填写加入拼车的信息
+     * @param inCarMsg  在拼车中的留言——为空则为null
+     * @param QQNum     QQ号——为空则为null
+     * @param WXNum     微信号——为空则为null
+     * @param phoneNum  手机号——为空则为null
+     * @param email 拼车结果通知邮箱——为空则为null
+     * @return 返回拼车状态
+     */
+    @RequestMapping("joinCarPooling/{carId}/{inCarMsg}/{QQNum}/{WXNum}/{phoneNum}/{email}")
+    public Map<String,Object> joinCarPooling(String carId,String inCarMsg,String QQNum,String WXNum,String phoneNum,String email);
+
+    /**
+     * 查看我的拼车信息——未完结的，当前还在拼车的信息
+     * @return
+     */
+    @RequestMapping("findMyCarPooling")
+    public Map<String,Object> findMyCarPooling();
+
+
+    /**
+     * 查看详细拼车信息
+     *      ————如果没有加入拼车，相应信息会进行保密处理
+     *      ————如果加入拼车，相应信息会进行展示
+     * @param carId
+     * @return
+     */
+     @RequestMapping("findDetailCarPooling/{carId}")
+    public Map<String,Object> findMyDetailCarPooling(String carId);
+
+
+    /**
+     * 查看所有历史拼车信息
+     * @return
+     */
+    @RequestMapping("findHistoryCarPooling")
+    public Map<String,Object> findHistoryCarPooling();
+
+    /**
+     * 退出拼车
+     * @param carId
+     * @return
+     */
+    @RequestMapping("quitCarPooling/{carId}")
+    public Map<String,Object> quitCarPooling(String carId);
+
+
+
+
+
+
+
 
 }
